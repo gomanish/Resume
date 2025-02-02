90f90b5e549923826718e805bdc4b59880fbf84f
Manish <krmanish2101@gmail.com>
2025-02-03 00:50:36 +0530

    Removing unused file

--- deprecated/js/app.js
+++
@@ -1,7 +0,0 @@
-const responsiveMenu = document.getElementById('menu-tab');
-const navBar = document.querySelector('.header');
-navBar.addEventListener('click',navMenu);
-responsiveMenu.addEventListener('click',navMenu);
-function navMenu(){
-    navBar.classList.toggle('responsive-nav');
-}
